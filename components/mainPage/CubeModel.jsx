164dcaea9172a0f252c4af8011ae7f2be8b63418
JamieLoLo <bleach7408@hotmail.com>
2024-08-21 20:41:19 +0800

    [FIX]model import

--- components/mainPage/CubeModel.jsx
+++ components/mainPage/CubeModel.jsx
@@ -1,18 +1,18 @@
-// components/CubeModel.js
 'use client'
 
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useLoader } from '@react-three/fiber'
 import { OrbitControls, useGLTF, Center } from '@react-three/drei'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default function CubeModel() {
   const { scene } = useGLTF('/model/Cube.glb')
+  // const { scene } = useLoader(GLTFLoader, '')
 
   return (
     <div className='w-full h-full overscroll-none'>
       <Canvas>
         <ambientLight intensity={3} />
         <OrbitControls enableZoom={false} />
-        {/* 使用 Center 组件将模型居中 */}
         <Center>
           <primitive object={scene} scale={1.4} />
         </Center>
