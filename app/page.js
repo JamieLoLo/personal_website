249331777c5ebddd3aba2b080d34d1ecf6b85561
JamieLoLo <bleach7408@hotmail.com>
2024-08-26 02:43:13 +0800

    [DELETE]unnecessary code

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
+'use client'
+
 import CubeModel from '@/components/mainPage/CubeModel'
 import Image from 'next/image'
+import { isMobile } from 'react-device-detect'
 
 export default function Home() {
   return (
-    <main className='w-screen h-[100dvh] bg-mainGrey2-100 main relative overscroll-none'>
-      <a
-        href='/blog'
-        className='fixed top-[20px] right-[20px] z-10 bg-white px-[20px] py-[5px]'
-      >
-        GO BLOG
-      </a>
-      <div className='w-full h-full absolute top-0 left-0 bg-mainGrey2-100/90 '></div>
+    <main className='w-screen h-[100dvh] relative overscroll-none'>
       <CubeModel />
     </main>
   )
