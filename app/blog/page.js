f666cca3728b80dfdf79a62f001b6337ecfe7b6b
JamieLoLo <bleach7408@hotmail.com>
2024-08-27 02:23:55 +0800

    [UPLOAD]optimize for pros

--- app/blog/page.js
+++ app/blog/page.js
@@ -52,24 +52,19 @@ export default function Blog() {
       </SessionProviderWrapper>
       <div className='w-[85%]   h-[calc(100%-60px)] mt-[60px] relative flex'>
         <div className='w-[70%] pl-[2.5vw] h-full relative py-[5%] pr-[5%] overflow-y-scroll overscroll-none'>
-          <SessionProviderWrapper>
-            {articles.data &&
-              articles.data.map((item, index) => (
-                <div key={item.id} className='w-full '>
-                  <PostPreview
-                    index={index}
-                    title={item.title}
-                    content={item.content}
-                    coverImage={item.coverImage}
-                    id={item.id}
-                    onRefresh={handleDataRefresh}
-                  />
-                  {Number(index) !== articles.data.length - 1 && (
-                    <div className='border-t w-full my-[20px]'></div>
-                  )}
-                </div>
-              ))}
-          </SessionProviderWrapper>
+          {articles.data &&
+            articles.data.map((item, index) => (
+              <div key={item.id} className='w-full '>
+                <PostPreview
+                  index={index}
+                  article={item}
+                  onRefresh={handleDataRefresh}
+                />
+                {Number(index) !== articles.data.length - 1 && (
+                  <div className='border-t w-full my-[20px]'></div>
+                )}
+              </div>
+            ))}
         </div>
         <div className='w-[30%] h-full relative '>
           <RightList categories={categories.data} />
