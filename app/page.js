fd845ebcc0a61a20ba9ff438bc402dab6dfecab0
JamieLoLo <bleach7408@hotmail.com>
2024-08-29 20:37:47 +0800

    [UPDATE]homepage image

--- app/page.js
+++ app/page.js
@@ -7,10 +7,12 @@ import { uiState } from '@/lib/valtioState'
 import { useEffect, useState } from 'react'
 import { useSnapshot } from 'valtio'
 import Ticker from '@/components/mainPage/Ticker'
+import { windowSizeState } from '@/lib/windowSize'
 
 export default function Home() {
   const { introVisible } = useSnapshot(uiState.introPage)
   const { loadingVisible } = useSnapshot(uiState.loading)
+  const { mobileMode } = useSnapshot(windowSizeState)
   const [showTicker, setShowTicker] = useState(false)
 
   useEffect(() => {
@@ -60,21 +62,26 @@ export default function Home() {
       <AnimatePresence>
         {introVisible && (
           <motion.div
-            className='w-full h-full absolute top-0 left-0 z-40 bg-black/40'
+            className='w-full h-full absolute top-0 left-0 z-40 bg-black/40 pointer-events-auto  '
             initial={{ opacity: 1 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5 }}
+            onClick={(e) => {
+              e.stopPropagation()
+            }}
           >
             <div className='w-full h-auto absolute top-[20%] left-[5%]'>
               <Image
-                src='/images/intro.png'
+                src={
+                  mobileMode ? '/images/intro_ph.png' : '/images/intro_pc.png'
+                }
                 alt='intro'
                 width={0}
                 height={0}
                 sizes='100vw'
                 priority
-                className=' w-[80%] h-auto object-contain'
+                className=' w-[80%] portraitPh:w-[90%] h-auto object-contain'
               />
 
               <Image
@@ -84,7 +91,7 @@ export default function Home() {
                 height={0}
                 sizes='100vw'
                 priority
-                className='mt-[150px] w-[150px] h-auto object-contain border-2 p-4 cursor-pointer'
+                className='mt-[15%] portraitPad:mt-[20%] portraitPh:mt-[40%] w-[150px] h-auto object-contain border-2 p-4 cursor-pointer'
                 onClick={closeHandler}
               />
             </div>
