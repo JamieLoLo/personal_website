98d87d5c724a8a59218b138f2dd29926139a7111
JamieLoLo <bleach7408@hotmail.com>
2024-08-29 17:40:08 +0800

    [UPDATE]variable

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -1,17 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
 export const options = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME,
+  host: process.env.RDS_HOST,
   port: Number(process.env.DB_PORT),
   dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: process.env.NODE_ENV === 'production',
-    },
-  },
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  // dialectOptions: {
+  //   ssl: {
+  //     rejectUnauthorized: process.env.NODE_ENV === 'production',
+  //   },
+  // },
+  // logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  logging: console.log,
   migrationStorageTableName: 'migrations',
 }
 
