95c796872b85e32dd441753be3ea399ae33a937c
JamieLoLo <bleach7408@hotmail.com>
2024-09-01 21:26:16 +0800

    [UPDATE]modify delay time

--- components/mainPage/ProjectInfo.jsx
+++ components/mainPage/ProjectInfo.jsx
@@ -61,7 +61,7 @@ export default function ProjectInfo() {
       if (scrollContainerRef.current) {
         scrollContainerRef.current.scrollTop = 0
       }
-    }, 300)
+    }, 200)
   }
 
   return (
