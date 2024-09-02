0df101145ca8853ba49d1046bf267e37f97ff4cf
JamieLoLo <bleach7408@hotmail.com>
2024-09-03 02:16:23 +0800

    [UPDATE]add GA

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import Toast from '@/components/Toast'
 import ConfirmModal from '@/components/blog/ConfirmModal'
 import Loading from '@/components/Loading'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-XCPERNLLGT`}
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XCPERNLLGT', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </head>
       <body className='NotoSansR select-none'>
         {children}
         <Toast />
