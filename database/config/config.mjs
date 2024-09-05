262ba0606ec91a50c67d1b0c4b1c221e1275a321
JamieLoLo <bleach7408@hotmail.com>
2024-09-05 22:51:23 +0800

    [UPDATE]dynamic get aws rds password

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -1,18 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 export const options = {
   username: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
   database: process.env.RDS_DB_NAME,
   host: process.env.RDS_HOST,
   port: Number(process.env.DB_PORT),
   dialect: 'mysql',
-  // dialectOptions: {
-  //   ssl: {
-  //     rejectUnauthorized: process.env.NODE_ENV === 'production',
-  //   },
-  // },
-  // logging: process.env.NODE_ENV === 'development' ? console.log : false,
-  logging: console.log,
   migrationStorageTableName: 'migrations',
 }
 
--- db_connection.js
+++ db_connection.js
@@ -1,9 +1,18 @@
 import { Sequelize } from 'sequelize'
 import { options } from './database/config/config.mjs'
+import getRdsPassword from './lib/rds'
 
-const dbOptions = options
-dbOptions.dialectModule = require('mysql2')
+async function initSequelize() {
+  const password = await getRdsPassword() // 動態獲取密碼
+  const dbOptions = { ...options, password } // 將密碼動態地插入到 options 中
+  dbOptions.dialectModule = require('mysql2') // 確保使用 mysql2 模組
 
-const sequelize = new Sequelize(dbOptions)
+  const sequelize = new Sequelize(dbOptions)
+
+  return sequelize
+}
+
+// 動態創建 sequelize 實例
+const sequelize = await initSequelize()
 
 export default sequelize
