21eebccd6441dda41fc4025e3a52d809f4ee2da8
JamieLoLo <bleach7408@hotmail.com>
2025-04-17 13:06:30 +0800

    chore: delete unnecessary comment

--- app/page.js
+++ app/page.js
@@ -11,8 +11,6 @@ import { windowSizeState } from '@/lib/windowSize'
 import { isMobile as checkIsMobile } from 'react-device-detect'
 import { projectInfoData } from '@/database/projectInfoData'
 
-// test for git commit
-
 export default function Home() {
   const { introVisible } = useSnapshot(uiState.introPage)
   const { loadingVisible } = useSnapshot(uiState.loading)
